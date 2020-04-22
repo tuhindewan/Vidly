cdfe4d3cffaa7568f153a1605360b3d45956e9ae
tuhindewan <tuhinsshadow@gmail.com>
2020-04-22 15:51:17 +0600

    Customize Movie and Genre Apps for admin panel

--- movies/models.py
+++ movies/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
@@ -13,3 +16,6 @@ class Movie(models.Model):
     daily_rate = models.FloatField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
