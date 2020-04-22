9fbe3fe0b2959fb74ddfd815b78a04cc3482af45
tuhindewan <tuhinsshadow@gmail.com>
2020-04-22 17:23:30 +0600

    Retrive all movies and send it to index view

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World! You're at the polls index.")
+    movies = Movie.objects.all()
+    title_list = [movie.title for movie in movies]
+    separator = ', '
+    result = separator.join(title_list)
+    return HttpResponse(result)
