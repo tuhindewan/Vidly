d3a34910871478cde3af272e01c504176e252ff0
tuhindewan <tuhinsshadow@gmail.com>
2020-04-23 18:23:25 +0600

    Cretae index file and show data from database

--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Movie
-
-# Create your views here.
 
 
 def index(request):
     movies = Movie.objects.all()
-    title_list = [movie.title for movie in movies]
-    separator = ', '
-    result = separator.join(title_list)
-    return HttpResponse(result)
+    return render(request, 'movies/index.html', {'movies': movies})
