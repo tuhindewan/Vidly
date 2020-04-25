8773afc53e68ea5c9c56c0584d67d98fe37bb3bf
tuhindewan <tuhinsshadow@gmail.com>
2020-04-25 22:24:59 +0600

    Referencing URLs

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'movie'
+
 urlpatterns = [
-    path('', views.index, name='movie_index'),
-    path('<int:movie_id>', views.detail, name='movie_index')
+    path('', views.index, name='index'),
+    path('<int:movie_id>', views.detail, name='detail')
 ]
