54bea4d86e06e42162036c8a353b94a58dd95d1a
tuhindewan <tuhinsshadow@gmail.com>
2020-04-25 19:38:06 +0600

    -  Sharing Base Template Across Multiple Apps -  Create Deatil view funtion for single movie

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Movie
+from django.http import HttpResponse
 
 
 def index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    return HttpResponse(movie_id)
